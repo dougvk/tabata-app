3c3626c37f82af81ef573ba7d54e4fc911d1f226
dougvk <doug@swirldslabs.com>
2024-09-19 13:42:20 +0200

    Initial Commit

---
+++ TabataTrainer/TabataTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTrainerApp.swift
+//  TabataTrainer
+//
+//  Created by  Doug Von Kohorn on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TabataTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabataTrainerTests/TabataTrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTrainerTests.swift
+//  TabataTrainerTests
+//
+//  Created by  Doug Von Kohorn on 9/19/24.
+//
+
+import Testing
+@testable import TabataTrainer
+
+struct TabataTrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
